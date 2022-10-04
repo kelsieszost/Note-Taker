f0ff93ad7cdaa472678381104644c07c39ee3f0d
kelsieszost <owner@kelsieszost.design>
2022-10-04 14:54:25 -0400

    getnotes/savenote functions

--- public/assests/index.js
+++ public/assests/index.js
@@ -16,7 +16,25 @@ const show = (elem) => {
     elem.style.display = 'inline';
   };
   
-  // Hide an element
   const hide = (elem) => {
     elem.style.display = 'none';
   };
+
+  let newNote = {};
+
+const getNotes = () =>
+  fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
