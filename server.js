59f85dc3800b0776176c012680e8ce0fedf52034
kelsieszost <owner@kelsieszost.design>
2022-10-04 14:32:06 -0400

    created routing structure

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+var PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./public'));
+
+require('./routes/api_route')(app);
+require('./routes/html_route')(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on Port: ${PORT}`);
+});
